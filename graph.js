ce5f2ac3b5b4da3a2146885c47b077030b89dffd
LastLeaf <bqfu@163.com>
2014-09-02 18:10:11 +0800

    Added by-degree color.

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 // generated edge lists
-var graph = [["lightpalette","[ NO NAME ]"],["fw.mpa","[ NO NAME ]"],["compression","bytes"],["compression","compressible"],["compression","negotiator"],["fw.mpa","compression"],["mongodb","bson"],["mongodb","kerberos"],["connect-mongo","mongodb"],["fw.mpa","connect-mongo"],["cookie-parser","cookie"],["cookie-parser","cookie-signature"],["fw.mpa","cookie-parser"],["accepts","mime"],["accepts","negotiator"],["express","accepts"],["express","buffer-crc32"],["express","cookie"],["express","cookie-signature"],["express","debug"],["express","escape-html"],["express","fresh"],["express","merge-descriptors"],["express","methods"],["express","parseurl"],["express","path-to-regexp"],["proxy-addr","ipaddr.js"],["express","proxy-addr"],["express","qs"],["express","range-parser"],["send","debug"],["send","mime"],["express","send"],["express","serve-static"],["type-is","mime"],["express","type-is"],["express","utils-merge"],["fw.mpa","express"],["express-session","buffer-crc32"],["express-session","cookie"],["express-session","cookie-signature"],["debug","ms"],["express-session","debug"],["express-session","depd"],["express-session","on-headers"],["uid-safe","base64-url"],["uid-safe","mz"],["express-session","uid-safe"],["express-session","utils-merge"],["fw.mpa","express-session"],["handlebars","[ NO NAME ]"],["optimist","wordwrap"],["handlebars","optimist"],["uglify-js","async"],["source-map","amdefine"],["uglify-js","source-map"],["handlebars","uglify-js"],["fw.mpa","handlebars"],["fw.mpa","locale"],["fw.mpa","mkdirp"],["mongoose","hooks"],["bson","nan"],["mongodb","bson"],["mongodb","kerberos"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["mongodb","readable-stream"],["mongoose","mongodb"],["mongoose","mpath"],["mongoose","mpromise"],["mquery","debug"],["mongoose","mquery"],["mongoose","ms"],["mongoose","muri"],["mongoose","regexp-clone"],["mongoose","sliced"],["fw.mpa","mongoose"],["faye-websocket","websocket-driver"],["sockjs","faye-websocket"],["sockjs","node-uuid"],["fw.mpa","sockjs"],["stylus","css-parse"],["stylus","debug"],["glob","inherits"],["minimatch","lru-cache"],["minimatch","sigmund"],["glob","minimatch"],["stylus","glob"],["stylus","mkdirp"],["stylus","sax"],["fw.mpa","stylus"],["optimist","wordwrap"],["uglify-js","optimist"],["source-map","amdefine"],["uglify-js","source-map"],["uglify-js","uglify-to-browserify"],["fw.mpa","uglify-js"],["fw.mpa","uglifycss"],["fw.mpa","watch"],["lightpalette","fw.mpa"],["lightpalette","marked"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["multiparty","readable-stream"],["multiparty","stream-counter"],["lightpalette","multiparty"],["mv","[ NO NAME ]"],["mv","mkdirp"],["mv","ncp"],["mv","rimraf"],["lightpalette","mv"],["nodemailer","[ NO NAME ]"],["nodemailer","directmail"],["nodemailer","he"],["mailcomposer","dkim-signer"],["follow-redirects","underscore"],["mailcomposer","follow-redirects"],["mailcomposer","mime"],["mimelib","addressparser"],["encoding","iconv-lite"],["mimelib","encoding"],["mailcomposer","mimelib"],["mailcomposer","punycode"],["nodemailer","mailcomposer"],["nodemailer","public-address"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["nodemailer","readable-stream"],["simplesmtp","rai"],["simplesmtp","xoauth2"],["nodemailer","simplesmtp"],["lightpalette","nodemailer"],["lightpalette","pngjs"],["rss","mime"],["rss","xml"],["lightpalette","rss"],["sanitize-html","[ NO NAME ]"],["sanitize-html","he"],["htmlparser2","domelementtype"],["htmlparser2","domhandler"],["htmlparser2","domutils"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["htmlparser2","readable-stream"],["sanitize-html","htmlparser2"],["sanitize-html","lodash"],["lightpalette","sanitize-html"],["lightpalette","timezone"]];
+var graph = [["compression","bytes"],["compression","compressible"],["compression","negotiator"],["fw.mpa","compression"],["mongodb","bson"],["mongodb","kerberos"],["connect-mongo","mongodb"],["fw.mpa","connect-mongo"],["cookie-parser","cookie"],["cookie-parser","cookie-signature"],["fw.mpa","cookie-parser"],["accepts","mime"],["accepts","negotiator"],["express","accepts"],["express","buffer-crc32"],["express","cookie"],["express","cookie-signature"],["express","debug"],["express","escape-html"],["express","fresh"],["express","merge-descriptors"],["express","methods"],["express","parseurl"],["express","path-to-regexp"],["proxy-addr","ipaddr.js"],["express","proxy-addr"],["express","qs"],["express","range-parser"],["send","debug"],["send","mime"],["express","send"],["express","serve-static"],["type-is","mime"],["express","type-is"],["express","utils-merge"],["fw.mpa","express"],["express-session","buffer-crc32"],["express-session","cookie"],["express-session","cookie-signature"],["debug","ms"],["express-session","debug"],["express-session","depd"],["express-session","on-headers"],["uid-safe","base64-url"],["uid-safe","mz"],["express-session","uid-safe"],["express-session","utils-merge"],["fw.mpa","express-session"],["optimist","wordwrap"],["handlebars","optimist"],["uglify-js","async"],["source-map","amdefine"],["uglify-js","source-map"],["handlebars","uglify-js"],["fw.mpa","handlebars"],["fw.mpa","locale"],["fw.mpa","mkdirp"],["mongoose","hooks"],["bson","nan"],["mongodb","bson"],["mongodb","kerberos"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["mongodb","readable-stream"],["mongoose","mongodb"],["mongoose","mpath"],["mongoose","mpromise"],["mquery","debug"],["mongoose","mquery"],["mongoose","ms"],["mongoose","muri"],["mongoose","regexp-clone"],["mongoose","sliced"],["fw.mpa","mongoose"],["faye-websocket","websocket-driver"],["sockjs","faye-websocket"],["sockjs","node-uuid"],["fw.mpa","sockjs"],["stylus","css-parse"],["stylus","debug"],["glob","inherits"],["minimatch","lru-cache"],["minimatch","sigmund"],["glob","minimatch"],["stylus","glob"],["stylus","mkdirp"],["stylus","sax"],["fw.mpa","stylus"],["optimist","wordwrap"],["uglify-js","optimist"],["source-map","amdefine"],["uglify-js","source-map"],["uglify-js","uglify-to-browserify"],["fw.mpa","uglify-js"],["fw.mpa","uglifycss"],["fw.mpa","watch"],["lightpalette","fw.mpa"],["lightpalette","marked"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["multiparty","readable-stream"],["multiparty","stream-counter"],["lightpalette","multiparty"],["mv","mkdirp"],["mv","ncp"],["mv","rimraf"],["lightpalette","mv"],["nodemailer","directmail"],["nodemailer","he"],["mailcomposer","dkim-signer"],["follow-redirects","underscore"],["mailcomposer","follow-redirects"],["mailcomposer","mime"],["mimelib","addressparser"],["encoding","iconv-lite"],["mimelib","encoding"],["mailcomposer","mimelib"],["mailcomposer","punycode"],["nodemailer","mailcomposer"],["nodemailer","public-address"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["nodemailer","readable-stream"],["simplesmtp","rai"],["simplesmtp","xoauth2"],["nodemailer","simplesmtp"],["lightpalette","nodemailer"],["lightpalette","pngjs"],["rss","mime"],["rss","xml"],["lightpalette","rss"],["sanitize-html","he"],["htmlparser2","domelementtype"],["htmlparser2","domhandler"],["htmlparser2","domutils"],["readable-stream","core-util-is"],["readable-stream","inherits"],["readable-stream","isarray"],["readable-stream","string_decoder"],["htmlparser2","readable-stream"],["sanitize-html","htmlparser2"],["sanitize-html","lodash"],["lightpalette","sanitize-html"],["lightpalette","timezone"]];
 
 'use strict'; (function(){
 
@@ -7,9 +7,9 @@ var graph = [["lightpalette","[ NO NAME ]"],["fw.mpa","[ NO NAME ]"],["compressi
 	var HEIGHT = window.innerHeight;
 
 	var V_CIRCLE_R = 5;
-	var V_CIRCLE_COLOR = '#00d2ff';
+	var V_CIRCLE_COLOR_H = '191';
+	var V_CIRCLE_COLOR_S = '100%';
 	var E_SINGLE_COLOR = '#bbb';
-	var E_DOUBLE_COLOR = '#444';
 
 	var E_DISTANCE = 10;
 	var E_CHARGE = -200;
@@ -22,11 +22,19 @@ var graph = [["lightpalette","[ NO NAME ]"],["fw.mpa","[ NO NAME ]"],["compressi
 		vObj[graph[i][1]] = 0;
 	}
 	for(var k in vObj) {
-		v.push({title: k});
+		v.push({title: k, degree: 0});
 		vObj[k] = v.length - 1;
 	}
 	for(var i=0; i<graph.length; i++) {
-		e.push({ source: vObj[graph[i][0]], target: vObj[graph[i][1]] })
+		var v0 = vObj[graph[i][0]];
+		var v1 = vObj[graph[i][1]];
+		v[v0].degree++;
+		v[v1].degree++;
+		e.push({ source: v0, target: v1 });
+	}
+	var degreeMax = 1;
+	for(var i=0; i<v.length; i++) {
+		if(v[i].degree > degreeMax) degreeMax = v[i].degree;
 	}
 
 	// force graph
@@ -57,7 +65,9 @@ var graph = [["lightpalette","[ NO NAME ]"],["fw.mpa","[ NO NAME ]"],["compressi
 				.attr('cx', 0)
 				.attr('cy', 0)
 				.attr('r', V_CIRCLE_R)
-				.style('fill', V_CIRCLE_COLOR);
+				.each(function(d){
+					d3.select(this).style('fill', 'hsl(' + V_CIRCLE_COLOR_H + ',' + V_CIRCLE_COLOR_S + ',' + (75 - d.degree/degreeMax*50) + '%)');
+				});
 		})
 		.call(force.drag);
 
@@ -72,18 +82,7 @@ var graph = [["lightpalette","[ NO NAME ]"],["fw.mpa","[ NO NAME ]"],["compressi
 		})
 		.on('mouseleave', function(){
 			d3.select('#tooltip').style('display', 'none');
-		})
-		/*.on('mousedown', function(){
-			force.stop();
-			d3.select('#tooltip .tooltipText').text(this.getAttribute('vtitle'));
-		})
-		.on('mouseup', function(){
-			force.resume();
-		})
-		.on('mousemove', function(e){
-			this.data.x = e.layerX;
-			this.data.y = e.layerY;
-		})*/;
+		});
 
 	// draw lines
 	svgLines.selectAll('line').data(e).enter().append('line')
--- node_dep.js
+++ node_dep.js
@@ -3,20 +3,20 @@ var fs = require('fs');
 var graph = [];
 
 var parseDir = function(basepath){
-	var curProject = '[ NO NAME ]';
+	var curProject = '';
 	if(fs.existsSync(basepath + 'package.json') && !fs.statSync(basepath + 'package.json').isDirectory()) {
-		var curProject = JSON.parse(fs.readFileSync(basepath + 'package.json').toString('utf8')).name || '[ NO NAME ]';
+		curProject = JSON.parse(fs.readFileSync(basepath + 'package.json').toString('utf8')).name || '';
 	}
 	if(fs.existsSync(basepath + 'node_modules') && fs.statSync(basepath + 'node_modules').isDirectory()) {
 		fs.readdirSync(basepath + 'node_modules').forEach(function(dir){
 			if(fs.statSync(basepath + 'node_modules/' + dir).isDirectory()) {
 				var baseProject = parseDir(basepath + 'node_modules/' + dir + '/');
-				graph.push([curProject, baseProject]);
+				if(baseProject) graph.push([curProject || '[ NO NAME ]', baseProject]);
 			}
 		});
 	}
 	return curProject;
-}
+};
 parseDir('./');
 
 console.log(JSON.stringify(graph));
